daeb67abc916e7b727f7479df8f3970d82453ceb
Felipe Boni <felipeboni@4x4brasil.com.br>
2023-03-03 11:55:55 -0300

    updated some texts

--- src/components/modules/_techs.jsx
+++ src/components/modules/_techs.jsx
@@ -20,17 +20,14 @@ export const Techs = () => {
               experiences.
             </p>
             <p className="max-w-xl mb-5 text-white">
-              Currently, I work at Shopify as a Senior UX Developer
+              Currently, I work at 4x4Brasil as a UI Designer And Front End Developer
               and Accessibility advocate crafting thoughtful and inclusive
               experiences that adhere to web standards for over a million
               merchants across the world.
             </p>
             <p className="max-w-xl text-white">
-              Before now, I was Principal Frontend
-              Engineer at hellotax, where I worked on a suite of tools and
-              services tailored at providing fast, automated VAT Registration /
-              filings &amp; Returns solutions for multi-channel sellers across
-              Europe.
+              Before now, I was a Freelancer Designer
+              where I created visual solutions to meet various business or individual needs, such as graphics, illustrations, layouts, and user interfaces, for various mediums like print, digital, or web. I worked closely with clients or project managers to understand their requirements and goals, conduct research, create sketches, develop design concepts, and produce final designs.
             </p>
           </div>
         </article>
